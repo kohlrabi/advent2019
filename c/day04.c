3b75760bd17c592741ce4f88ea99d344ba763ca5
Jan Christoph Terasa <christoph@kohlio.de>
2019-12-10 08:35:29 +0100

    Solved day 4, pt 2.

--- c/day04.c
+++ c/day04.c
@@ -4,7 +4,7 @@
 const int min = 367479;
 const int max = 893698;
 
-int password(int p)
+int part1(int p)
 {
 	int i;
 	char s[7];
@@ -22,17 +22,45 @@ int password(int p)
 	return two;
 }
 
+int part2(int p)
+{
+	int i;
+	char s[7];
+	int v, l;
+
+	snprintf(s, 7, "%d", p);
+	v = s[0];
+	l = 1;
+	for(i=1; i<6; i++) {
+		if(s[i] == v) {
+			l += 1;
+		}
+		else {
+			if(l == 2) {
+				return 1;
+			}
+			v = s[i];
+			l = 1;
+		}
+	}
+	return l == 2;
+}
+
 int main(int argc, char **argv)
 {
-	int i, s = 0;
+	int i, s = 0, s2 = 0;
 
 	for(i=min; i<max+1; i++) {
-		if(password(i)) {
+		if(part1(i)) {
 			s++;
+			if(part2(i)) {
+				s2++;
+			}
 		}
 	}
 
 	printf("part1: %d\n", s);
+	printf("part2: %d\n", s2);
 
 	return 0;
 }
